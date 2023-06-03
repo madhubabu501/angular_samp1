cf5ec1d812999c8ef67d3cb78b6f1c7ec71d733e
madhubabu501 <135126092+madhubabu501@users.noreply.github.com>
2023-06-03 13:31:15 +0530

    login some dflt chngs

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit{
-loginform !: FormGroup;
-constructor(private formBuilder:FormBuilder){}
-ngOnInit(): void {
-  this.loginform = this.formBuilder.group({
-    userName:['', Validators.required],
-    passWord:['',Validators.required]
-  })
-}
+export class LoginComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+  }
 }
