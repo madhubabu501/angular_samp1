9f7a43c28ee33562664ae66f1dd476592197df1d
madhubabu501 <135126092+madhubabu501@users.noreply.github.com>
2023-06-15 12:10:07 +0530

    dob month change in signupform

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -38,5 +38,26 @@ export class SignupComponent implements OnInit {
   signup() {
     console.log(this.signupForm.value);
   }
+  monthChange() {
+    // console.log(this.signupForm.controls['dob_month'].value == this.monthArray[0]);
+    // console.log(this.signupForm.controls['dob_month'].value);
+
+    if (this.signupForm.controls['dob_month'].value == this.monthArray[0] || this.signupForm.controls['dob_month'].value == this.monthArray[2] || this.signupForm.controls['dob_month'].value == this.monthArray[4] || this.signupForm.controls['dob_month'].value == this.monthArray[6] || this.signupForm.controls['dob_month'].value == this.monthArray[7]
+      || this.signupForm.controls['dob_month'].value == this.monthArray[9] || this.signupForm.controls['dob_month'].value == this.monthArray[11]) {
+      this.dayArray = [];
+      this.getday();
+    } else if (this.signupForm.controls['dob_month'].value == this.monthArray[1]) {
+      this.dayArray = [];
+      for (let i = 1; i <= 28; i++) {
+        this.dayArray.push(i);
+      }
+    } else if (this.signupForm.controls['dob_month'].value == this.monthArray[3] || this.signupForm.controls['dob_month'].value == this.monthArray[5]
+      || this.signupForm.controls['dob_month'].value == this.monthArray[8] || this.signupForm.controls['dob_month'].value == this.monthArray[10]) {
+      this.dayArray = [];
+      for (let i = 1; i <= 30; i++) {
+        this.dayArray.push(i);
+      }
+    }
+  }
 
 }
